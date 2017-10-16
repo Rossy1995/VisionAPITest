22aa10fedb9580bb5d76ccc9bc003bf6ca374732
Rossy1995 <lordross11@googlemail.com>
2017-10-16 16:08:55 +0100

    Load image from gallery into ImageView.

--- app/src/main/java/com/example/rossmaguire/visionapitest/MainActivity.java
+++ app/src/main/java/com/example/rossmaguire/visionapitest/MainActivity.java
@@ -1,111 +1,22 @@
 package com.example.rossmaguire.visionapitest;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 
-import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String imageFilePath;
-    private static int RESULT_LOAD_IMAGE = 1;
-
-    public static final String subscriptionKey = "20bfc04b498d4a4ab03d255ccc572e4b";
-    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/models/landmarks/analyze";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnImage = (Button) findViewById(R.id.btnImage);
-
-        btnImage.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View arg0)
-            {
-                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
-            }
-        });
-
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            Log.i("", picturePath);
-            cursor.close();
-
-            ImageView imgLandmark = (ImageView) findViewById(R.id.imgLandmark);
-            imgLandmark.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-
-        }
-    }
-
-    public void analyseImage(String imageFilePath)
-    {
-        HttpClient httpClient = new DefaultHttpClient();
-
-        try
-        {
-            HttpPost request = new HttpPost(uriBase);
-
-            // Request headers.
-            request.setHeader("Content-Type", "application/json");
-            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-
-            // Request body.
-            StringEntity requestEntity = new StringEntity("{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/2/23/Space_Needle_2011-07-04.jpg\"}");
-            request.setEntity(requestEntity);
-
-            // Execute the REST API call and get the response entity.
-            HttpResponse response = httpClient.execute(request);
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null)
-            {
-                // Format and display the JSON response.
-                String jsonString = EntityUtils.toString(entity);
-                JSONObject json = new JSONObject(jsonString);
-                System.out.println("REST Response:\n");
-                System.out.println(json.toString(2));
-            }
-        }
-        catch (Exception e)
-        {
-            // Display error message.
-            System.out.println(e.getMessage());
-        }
+    public void activityAnalyzeInDomain(View v) {
+        Intent intent = new Intent(this, AnalyseImageActivity.class);
+        startActivity(intent);
     }
 }
